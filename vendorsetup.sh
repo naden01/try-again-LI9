4fd10e5b56a8158b062a9cc596726399f386a5b1
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-07-09 09:18:13 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp-LI9-user
-add_lunch_combo twrp-LI9-userdebug
-add_lunch_combo twrp-LI9-eng
+add_lunch_combo twrp_LI9-user
+add_lunch_combo twrp_LI9-userdebug
+add_lunch_combo twrp_LI9-eng
