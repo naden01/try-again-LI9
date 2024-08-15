3e5379f9042f2408d2a6f1e2730f10ed80057afd
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-08-15 02:54:28 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -8,6 +8,29 @@
 LOCAL_PATH := device/tecno/LI9
 
 #AB
+AB_OTA_UPDATER := true
+ENABLE_VIRTUAL_AB := true
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    gz \
+    lk \
+    logo \
+    md1img \
+    preloader \
+    product \
+    scp \
+    spmfw \
+    sspm \
+    system \
+    system_ext \
+    tee \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor \
+    vendor \
+    vendor_boot
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
@@ -34,3 +57,8 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Mtk plpath utils
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery
