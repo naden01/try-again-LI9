97e130e0395c995ce24fd5168fbf2fc6522a1168
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-08-15 02:47:22 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,8 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/tecno/TECNO-LI9
-# A/B
+LOCAL_PATH := device/tecno/LI9
+
+#AB
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
